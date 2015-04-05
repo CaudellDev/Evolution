5c0a377d78033dff4d6de555c5e5006ca5edf851
Tyler <Tyler@192.168.1.107>
2015-04-04 21:05:19 -0500

    Created base project.

---
+++ LWJGL_Snake/src/lwjgl_snake/LWJGL_Snake.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lwjgl_snake;
+
+/**
+ *
+ * @author Tyler
+ */
+public class LWJGL_Snake {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
